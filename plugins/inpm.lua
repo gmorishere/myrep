5b0141da81fdda65cb541f8e85bd97c1e2b5cb6c
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2015-12-13 21:21:56 +0330

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -10,7 +10,7 @@ function run(msg, matches)
 		    return 'شما بن گلوبال شدید'
       end
       if data[tostring(matches[2])]['settings']['lock_member'] == 'yes' and not is_owner2(msg.from.id, matches[2]) then
-        return 'Group is private.'
+        return 'گروه شخصی است'
       end
       local chat = "chat#id"..matches[2]
 		  local user = "user#id"..msg.from.id
